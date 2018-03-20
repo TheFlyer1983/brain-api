326b98343539a3a3ce7a791924099ebd4c66d13a
TheFlyer1983 <theflyer1983@gmail.com>
2018-03-20 14:47:20 +0000

    Connected Database and Server

--- server.js
+++ server.js
@@ -1,106 +1,35 @@
+//Packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
-
+const knex = require('knex');
+//Controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+//DB Connection
 const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'Paul',
-    password : '',
+    password : 'Lingbeck1',
     database : 'smart-brain'
   }
 });
-
+//App Declaration
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
-app.get('/', (req, res) => {
-  res.send(database.users);
-})
-
-app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('Error Logging In');
-  }
-})
-
-app.post('/register', (req, res) => {
-  const {email, name, password } = req.body;
-  // bcrypt.hash(password, null, null, function(err, hash) {
-  //     console.log(hash);
-  // });
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
-    })
-    .then(user => {
-      res.json(user[0]);
-    })
-    .catch(err => res.status(400).json('Unable to Register'))
-})
-
-app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
-  db.select('*').from('users').where({id})
-  .then(user => {
-    if (user.length) {
-      res.json(user[0])
-    } else {
-      res.status(400).json('Not Found')
-    }
-  })
-  .catch(err => res.status(400).json('Error Getting User'))
-})
-
-app.put('/image', (req, res) => {
-  const {id} = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++
-      return res.json(user.entries);
-    }
-  })
-  if (!found) {
-    res.status(400).json('Not Found');
-  }
-})
-
-
-// Load hash from your password DB.
-
+//End Points
+app.get('/', (req, res) => {res.send(database.users)})
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(3000, () => {
   console.log('App is Running on Port 3000');
