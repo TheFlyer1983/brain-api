480d82ec465628b1fde04935cdac6e6724992291
TheFlyer1983 <theflyer1983@gmail.com>
2018-03-20 08:39:08 +0000

    Connecting Front End to database

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'Paul',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -46,28 +57,30 @@ app.post('/register', (req, res) => {
   // bcrypt.hash(password, null, null, function(err, hash) {
   //     console.log(hash);
   // });
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length-1]);
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('Unable to Register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const {id} = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('Not Found')
     }
   })
-  if (!found) {
-    res.status(400).json('Not Found');
-  }
+  .catch(err => res.status(400).json('Error Getting User'))
 })
 
 app.put('/image', (req, res) => {
