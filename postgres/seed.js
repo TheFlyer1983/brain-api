666f61d03e063a72a48df3842bcee3015de46206
TheFlyer1983 <theflyer1983@gmail.com>
2021-11-25 14:06:31 +0000

    Trying to add seed tables.

---
+++ postgres/seed.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const client = new pg.Client(connectionString);
+
+client.connect();
+client.query(`
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  name VARCHAR(100),
+  email text UNIQUE NOT NULL,
+  entries BIGINT DEFAULT 0,
+  joined TIMESTAMP NOT NULL
+)
+`, (err, res) => {
+  if (err) {
+    return console.error('Error with PostgreSQL Database', err);
+  }
+  client.end();
+});
