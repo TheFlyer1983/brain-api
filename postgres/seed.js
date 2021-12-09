32739095eea26f30d48f8f8dbcfd362fe448ee83
TheFlyer1983 <theflyer1983@gmail.com>
2021-12-09 15:59:25 +0000

    Adding multiple tables

--- postgres/seed.js
+++ postgres/seed.js
@@ -1,7 +1,6 @@
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL;
-console.log('Seed script running');
-console.log(process.env.DATABASE_URL);
+
 const client = new pg.Client(connectionString);
 
 
@@ -14,6 +13,19 @@ CREATE TABLE IF NOT EXISTS users (
   email text UNIQUE NOT NULL,
   entries BIGINT DEFAULT 0,
   joined TIMESTAMP NOT NULL
+)
+`, (err, res) => {
+  if (err) {
+    return console.error('Error with PostgreSQL Database', err);
+  }
+  client.end();
+});
+
+client.query(`
+CREATE TABLE IF NOT EXISTS login (
+  id serial PRIMARY KEY,
+  hash VARCHAR(100) NOT NULL,
+  email text UNIQUE NOT NULL
 )
 `, (err, res) => {
   if (err) {
