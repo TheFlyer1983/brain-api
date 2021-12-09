afb075835461a9d3ae232eb550d22162249f6d94
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2021-12-09 16:54:27 +0000

    Dev-op/adding-new-stack (#55)
    * trying to seed the db
    
    * hoping this is to do with the path.
    
    * updating path again
    
    * another attempt to run the script.
    
    * adding debugging.
    
    * Try running without docker
    
    * Adding multiple tables
    
    * changing the termination
    
    * trying to add multiple tables
    
    * more config
    
    * This time nesting the queries

---
+++ postgres/seed.js
@@ -0,0 +1,32 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const client = new pg.Client(connectionString);
+
+client.connect();
+client.query(`
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  name VARCHAR(100),
+  email text UNIQUE NOT NULL,
+  entries BIGINT DEFAULT 0,
+  joined TIMESTAMP NOT NULL
+)
+`, (err, res) => {
+  if (err) {
+    return console.error('Error with PostgreSQL Database', err);
+  }
+  client.query(`
+    CREATE TABLE IF NOT EXISTS login (
+      id serial PRIMARY KEY,
+      hash VARCHAR(100) NOT NULL,
+      email text UNIQUE NOT NULL
+    )
+  `, (err, res) => {
+    if (err) {
+      return console.error('Error with PostgreSQL Database', err);
+    }
+    client.end();
+  });
+});
+
