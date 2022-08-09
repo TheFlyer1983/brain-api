2ac67f1511ec3183594e4372862a2a605be5e3e3
theflyer1983 <theflyer1983@gmail.com>
2022-08-09 16:17:41 +0100

    Installed Axios replaced `node-fetch`

--- controllers/rank.js
+++ controllers/rank.js
@@ -1,17 +1,21 @@
-const fetch = require('node-fetch');
+const axios = require('axios').default;
 const { apiConfig, endpoints } = require('../constants');
+
+const client = axios.create();
 
 const getRank = async (req, res) => {
   const { entries } = req.body;
   try {
-    const response = await fetch(`${apiConfig.rank}${endpoints.rank}?rank=${entries}`);
-    const result = await response.json(response);
-    return await res.json(result.input);
+    const response = await client.get(
+      `${apiConfig.rank}${endpoints.rank}?rank=${entries}`
+    );
+
+    return await res.json(response.data.input);
   } catch (error) {
     return await res.status(400).json(error);
   }
 };
 
 module.exports = {
-  getRank,
+  getRank
 };
